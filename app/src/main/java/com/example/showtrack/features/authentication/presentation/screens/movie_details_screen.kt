ee6e41777a3e0a292db70b979f00e01d1bea2742
hebaalhomsi9 <h.alhomsi@studenti.unibs.it>
2025-05-15 09:05:19 +0200

    screen

--- app/src/main/java/com/example/showtrack/features/authentication/presentation/screens/movie_details_screen.kt
+++ app/src/main/java/com/example/showtrack/features/authentication/presentation/screens/movie_details_screen.kt
@@ -26,7 +26,7 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.example.showtrack.R // مهم لاستدعاء الموارد المحلية
+import com.example.showtrack.R 
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
